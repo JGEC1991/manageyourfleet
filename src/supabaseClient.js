d1aaf4315dfadd138a90e9c4f79f5ed03fe5f30e
JGEC1991 <89346164+JGEC1991@users.noreply.github.com>
2025-04-08 01:11:19 -0600

    Initial commit from your app

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-    const supabaseUrl = 'https://wlonzbzmltroejjvccts.supabase.co'
-    const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indsb256YnptbHRyb2VqanZjY3RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwODA2MTEsImV4cCI6MjA1OTY1NjYxMX0.RkiyWxdCeHldtTG_g4LpdMZtsAVbAEZnkSc-9RyfwnI'
+    const supabaseUrl = 'https://jozlsnsnruzibeaxfapu.supabase.co'
+    const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvemxzbnNucnV6aWJlYXhmYXB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwOTQ3MjQsImV4cCI6MjA1OTY3MDcyNH0.RMFxoGJnhuFDRcnH9VerHFC03UdM1zCFO3lMtvnwgRM'
 
     export const supabase = createClient(supabaseUrl, supabaseAnonKey)
